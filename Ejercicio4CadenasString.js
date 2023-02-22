96ecc335762f4aa31188f0017106e84886f00ba7
Leo-Mastrangelo <109768179+Leo-Mastrangelo@users.noreply.github.com>
2023-02-22 19:15:49 -0300

    Add files via upload

---
+++ Ejercicio4CadenasString.js
@@ -0,0 +1,11 @@
+//Ejercicio CadenasString
+let nombre = "Leo"
+let apellido = "Mastra"
+let estudiante = nombre + " " + apellido
+let estudianteMayus = estudiante.toUpperCase ()
+let estudianteMinus = estudiante.toLowerCase ()
+console.log (estudiante.length)
+console.log (nombre.charAt(0))
+console.log (apellido[5])
+let estudianteSinEspacios = nombre + apellido
+console.log (estudiante.includes(nombre))
